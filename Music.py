5cedfabecfd258445e157c62f3a81f0d0db159f3
Charleschakka3 <46946544+Charleschakka3@users.noreply.github.com>
2019-04-22 20:20:32 -0400

    Update Music.py

--- Music.py
+++ Music.py
@@ -1,4 +1,4 @@
 <!-- Wikplayer https://www.wikplayer.com -->
 <script type="text/javascript" src="https://www.wikplayer.com/code.js"
-data-config="{'skin':'skins/wikmini/funkyMiniDeepOcean/skin.css','volume':95,'autoplay':true,'shuffle':false,'repeat':2,'showcomment':false,'marqueetexton':true,'placement':'mini','showplaylist':false,'playlist':[{'title':'Peaceful%20Music%20','url':'https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DIIfw9kAbMlc'}]}" ></script>
+data-config="{'skin':'skins/wikfull/funkyDeepOcean/skin.css','volume':95,'autoplay':true,'shuffle':true,'repeat':2,'showcomment':false,'marqueetexton':true,'placement':'top','showplaylist':false,'playlist':[{'title':'Girls%20Like%20You%20ft.%20Cardi%20B','url':'https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DaJOTlE1K90k'}]}" ></script>
 <!-- Wikplayer code end -->
